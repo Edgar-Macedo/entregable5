341f7d48a45d08be05c19deab2547a2709cd2749
Edgar-Macedo <95006899+Edgar-Macedo@users.noreply.github.com>
2022-08-02 21:46:42 -0500

    Update PokeNav.jsx
    asd

--- src/components/PokeNav.jsx
+++ src/components/PokeNav.jsx
@@ -6,7 +6,7 @@ const PokeNav = () => {
     return (
         <div>
             <nav>
-                <img src="https://github.com/Edgar-Macedo/entregable5/blob/master/src/images/image12.png?raw=true" alt="pokedex" className='logo'/>
+                <img src="../src/images/image12.png" alt="pokedex" className='logo'/>
                 <div className='red-bar-inner'>&nbsp;</div>
                 <div className='black-bar-inner'>&nbsp;</div>
                 <div className="pokeball-inner">
