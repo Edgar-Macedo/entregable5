3e163039df44ab6f8e95598ff3635a4a134080d1
Edgar-Macedo <edgar.ml0814@gmail.com>
2022-08-31 11:00:30 -0500

    Logo was not loading

--- src/components/PokeNav.jsx
+++ src/components/PokeNav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Pokeball from './Pokeball';
 import '../styles/poke-nav.css'
+import image12 from '../images/image12.png'
 
 const PokeNav = () => {
     return (
         <div>
             <nav>
-                <img src="../src/images/image12.png" alt="pokedex" className='logo'/>
+                <img src={image12} alt="pokedex" className='logo'/>
                 <div className='red-bar-inner'>&nbsp;</div>
                 <div className='black-bar-inner'>&nbsp;</div>
                 <div className="pokeball-inner">
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 import '../styles/user-input.css'
 import Pokeball from './Pokeball';
+import image11 from '../images/image11.png'
 
 
 const UserInput = () => {
@@ -23,7 +24,7 @@ const UserInput = () => {
         <div>
             <div className='hero'>
                 <div>
-                    <img src="../src/images/image11.png" alt="pokedex-logo" />
+                    <img src={image11} alt="pokedex-logo" />
                     <h1 style={{fontSize: "50px", color: "#FE1936"}}>Hello trainer!</h1>
                     <p style={{color:"#302F2F", fontWeight:"500", fontSize:"25px"}}>Enter your name to get started</p>
                     <form onSubmit={submit}>
